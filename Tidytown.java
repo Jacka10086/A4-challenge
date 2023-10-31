c1a3cc3e8392de48fc942c32cd3401f67ab6bd35
Jacka10086 <111729221+Jacka10086@users.noreply.github.com>
2023-10-31 02:02:33 +0000

    Update Tidytown.java

--- Tidytown.java
+++ Tidytown.java
@@ -1,33 +1,35 @@
-package Assignments.Com410.A4;
-import java.util.ArrayList;
-import java.util.Scanner;
+package Assignments.Com410_4A; // 定义包名
+import java.util.ArrayList;    // 导入ArrayList类
+import java.util.Scanner;       // 导入Scanner类
 
-public class TidyTown {
+public class TidyTown {        // 定义一个名为TidyTown的公共类
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // 主方法，程序的入口点
+        
+        ArrayList<String> volunteers = new ArrayList<String>(); // 创建一个存储志愿者名字的ArrayList
+        boolean finished = false; // 声明一个布尔变量，初始为false，用于控制循环是否结束
+        Scanner keyboard = new Scanner(System.in); // 创建一个Scanner对象用于从控制台读取输入
+        String name;      // 声明一个字符串变量存储读入的名字
+        int pos;          // 声明一个整数变量，用于确定名字在ArrayList中的位置
 
-        ArrayList<String> volunteers = new ArrayList<String>();
-        boolean finished = false;
-        Scanner keyboard = new Scanner(System.in);
-        String name;
-        int pos;
+        do {              // 使用do-while循环读取用户输入，直到输入为"xxx"
+            System.out.print("Enter a name or xxx to finish > "); // 提示用户输入名字或输入"xxx"结束
+            name = keyboard.nextLine(); // 读取用户输入的一行
 
-        do {
-            System.out.print("Enter a name or xxx to finish > ");
-            name = keyboard.nextLine();
-            if (name.equals("xxx"))
-                finished = true;
-            else {
-                pos = 0;
+            if (name.equals("xxx")) {    // 如果用户输入的是"xxx"
+                finished = true;         // 将finished设为true，意味着结束输入
+            } 
+            else {                       // 否则
+                pos = 0;                 // 初始化位置为0
+                // 使用while循环确定name应该在ArrayList的哪个位置，确保名字按字母顺序排列
                 while (pos < volunteers.size() && name.compareTo(volunteers.get(pos)) > 0) pos++;
-                volunteers.add(pos, name);
+                volunteers.add(pos, name); // 在正确的位置添加名字
             }
-        } while (!finished);
+        } while (!finished); // 如果finished为false，则继续循环
 
-        System.out.println("\nTidytown Volunteers\n-------------------\n");
-        for (String person : volunteers) {
-            System.out.println(person);
+        System.out.println("\nTidytown Volunteers\n-------------------"); // 打印标题
+        for (String person : volunteers) { // 使用增强for循环遍历志愿者名字列表
+            System.out.println(person);   // 打印每个志愿者的名字
         }
     }
-
 }
